5f0a7019568f116b5e52cc73e18a6daa2f2be226
Shen Yangang <shenyangang@acttao.com>
2014-12-16 16:17:47 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//author : younger shen
+//email  : younger.x.shen@gmail.com
+
+//this file is part of the  *do not tap white block js * html5 game
+
+(function(window){
+    
+
+        
+})(window);
