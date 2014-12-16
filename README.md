9aa789b244e00370c8a1ee3a8b3804eb21e7538b
Shen Yangang <shenyangang@acttao.com>
2014-12-16 16:22:06 +0800

    readme.d

---
+++ README.md
@@ -0,0 +1,3 @@
+block.js
+
+        this project implements a simple *do not tap white block* game to study html5 canvas 
---
+++ js/block.js
@@ -0,0 +1,4 @@
+//author : younger shen
+//email  : younger.x.shen@gmail.com
+
+// this file is part of *do not tap white block js html5 game*
